1e4ce423c8e1ede56b2ac243a5364c916086538c
adyasari46 <71820336+adyasari46@users.noreply.github.com>
2020-09-26 19:52:07 +0700

    Add files via upload

---
+++ 11180910000016/index.php
@@ -0,0 +1,56 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Web</title>
+	<link href="style/style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+	
+		<div id="header">
+			<div class="lingkaran"><img src="style/1.png" width="100" height="100" /></div>
+			<div class="judul">Adya Sari</div>
+			<div class="judul2">11180910000016</div>
+			<div class="kertas"></div>
+		</div>
+		
+		<div id="kertas">
+			<p>Suasana Study From Home</p>
+			<h1>Berawal dari merebaknya covid-19 sejak akhir 2019 di kota Wuhan, hingga kini hampir semua negara dibelahan dunia 
+			terdampak oleh pandemi ini. Sejak Februari 2020 lalu pandemi ini mulai masuk ke Indonesia, banyak sekolah dan instansi
+			memutuskan untuk "study/work from home" atau istilahnya belajar/bekerja dari rumah demi memutus mata rantai covid-19. Hal ini 
+			tentu menjadi suatu hal baru terutama bagi Indonesia, dengan WFH/SFH ini kita dapat mempelajari dan memanfaatkan perkembangan 
+			teknologi yang ada sekarang, tentunya akan mengurangi penduduk Indonesia yang gaptek. Dalam hal lain, bagi pribadi saya sendiri
+			SFH ini menjadi suatu hal yang menyenangkan sekaligus menyusahkan. Menyenangkan ketika kita tidak harus pergi ke kampus dan 
+			dapat berkumpul dirumah bersama keluarga, kuliah bisa sambil rebahan, dan masih banyak hal menyenangkan lainnya. Susahnya ialah
+			ketika ada kendala/gangguan sementara kita sedang kuliah, seperti gangguan sinyal dan lain-lain, terlebih lagi bagi anak kuliahan
+			yang jurusannya banyak praktik seperti kedokteran dan IT, saya rasa ini salah satu penyebab SFH menjadi kurang menyenangkan. </h1>
+		</div>
+		<div id="kertas2">
+			<p>Sarana/Fasilitas UIN Jakarta</p>
+			
+		</div>
+		<div id="kertas3">
+			<h1>1. Student Center</h1>
+			<img src="style/s.jpg" width="300" height="250"/>
+			<h1>Student Center merupakan sebuah gedung yang luas, yang di dalamnya terdapat stadion mini beserta lapangannya, mesjid, aula, koperasi, hingga sekretariat UKM.
+			Pilihan UKM yang ada di UIN Jakarta pun beragam, yang menyesuaikan dengan minat dan bakat. Ada kelompok pecinta alam, fotografi, olahraga, 
+			keislaman, musik dan padus, pramuka, kebahasaan, kesehatan, koperasi, teater, pers, hingga resimen mahasiswa. </h1>
+		</div>
+		<div id="kertas4">
+			<h1>2. Kafe Cangkir</h1>
+			<img src="style/c.jpg" width="300" height="250"/>
+			<h1> Kafe yang berbentuk cangkir raksasa ini menyediakan menu-menu yang beraneka macam seperti aneka nasi, sajian mie yang beragam, ayam, ikan, lauk pauk, dan lain-lain. Selain makanan, 
+			Kafe Cangkir juga dilengkapi minimarket yang menjual kebutuhan mahasiswa yang lengkap, baik berupa alat tulis hingga keperluan rumah.
+			Harganya yang bersahabat dengan dompet mahasiswa dan suasana yang cozy menambah rasa betah ketika berada di kampus.</h1>
+		</div>
+		<div id="kertas5">
+			<h1>3. Jalan Pesanggrahan</h1>
+			<img src="style/p.jpg" width="300" height="250"/>
+			<h1>Jalan ini dapat dikatakan sebagai pusat kehidupan mahasiswa UIN. Jalan yang terletak persis di samping kompleks Kampus I UIN Jakarta ini memberikan
+			pelayanan di luar kampus dengan menghadirkan puluhan pedagang kaki lima, tempat makan yang enak dan murah meriah,
+			kos-kosan maupun rumah kontrakan dengan harga terjangkau dan pastinya dekat dengan kampus. 
+			Sekretariat dan tempat perkumpulan beberapa organisasi juga ada di sini yang membuatnya tampak hidup selama 24 jam.</h1>
+		</div>
+		<div id="clear"></div>
+		<div id="page"></div>
+</body>
+</html>
